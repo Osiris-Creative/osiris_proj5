d03f878db3a525491d3606b34435f2e116ba6ffd
Caterina van Herpt <caterina.vanherpt@gmail.com>
2017-08-29 17:47:16 -0400

    updating ajax calls, index and gulp file

--- dev/scripts/scripts.js
+++ dev/scripts/scripts.js
@@ -1,13 +1,12 @@
-// function test() {
-// 	console.log("test");
-// }
+console.log("test");
+
 
 // create an empty object
 
 const app = {};
 
 app.yumKey = 'bf6ff579ddf44506f1a5ba19a2eb465a';
-app.yumId = 'f49c0671';
+app.yumId = 'fa8d9918';
 app.movieKey = 'dc85e0389c4e0355687d4c1bf7e0d2c1';
 
 // events handling
@@ -30,21 +29,34 @@ app.parseData = function(){
 // get data function
 
 app.getData = function(){
-	$.ajax({
+	let yumCallOne = $.ajax({
 		url:'http://api.yummly.com/v1/api/recipes',
-		method: 'Get',
+		method: 'GET',
 		dataType: 'json',
 		data:{
-			_app_key: app.yumKey,
 			_app_id: app.yumId,
-			q: 'cheese'
+			_app_key: app.yumKey,
+			q: 'onion soup',
+			maxResult: 10,
+			format: 'json'
 		}
-	}).then(function(res){
-		console.log(res);
-	}).fail (function(err) {
-		console.log(err)
+	  })
+	let movieCallOne = $.ajax({
+		url: 'https://api.themoviedb.org/3/discover/movie',
+		method: 'GET',
+		dataType: 'json',
+		data: {
+			api_key: app.movieKey,
+			with_genres: 35,
+			primary_release_year: 2010,
+			sort_by: 'vote_average.desc'
+		}
 	})
-	// let callMovie =
+	  
+	$.when(yumCallOne, movieCallOne).then(function (res1,res2){
+		console.log(res1,res2);
+	})
+
 };
 
 
